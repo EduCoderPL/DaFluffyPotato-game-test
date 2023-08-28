59b0b1e832f05cb48a4837eb91b583f21af58425
Arkadiusz Mazurek <66950904+AMFeniksPL@users.noreply.github.com>
2023-08-28 18:40:06 +0200

    Initial commit

---
+++ game.py
@@ -0,0 +1,88 @@
+import sys
+import pygame
+
+from scripts.entities import Player
+from scripts.utils import *
+from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Test Ninja Game")
+        self.screen = pygame.display.set_mode((1280, 960))
+        self.display = pygame.Surface((320, 240))
+
+
+        self.clock = pygame.time.Clock()
+
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+        self.movement = [False, False]
+
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur = 6),
+            'player/run': Animation(load_images('entities/player/run'), img_dur = 4),
+            'player/jump': Animation(load_images('entities/player/jump'), img_dur = 6),
+            'player/slide': Animation(load_images('entities/player/slide'), img_dur = 6),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide'), img_dur = 6),
+
+        }
+
+        self.clouds = Clouds(self.assets['clouds'], count = 128)
+        self.player = Player(self, (50, 50), (8, 15))
+
+        self.tilemap = Tilemap(self, tile_size=16)
+        self.tilemap.load('map.json')
+        self.scroll = [0, 0]
+
+
+    def run(self):
+        while True:
+
+            self.display.blit(self.assets['background'], (0, 0))
+
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset=render_scroll)
+
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display, offset=render_scroll)
+
+
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Game().run()
---
+++ scripts/tilemap.py
@@ -0,0 +1,98 @@
+
+AUTOTILE_MAP = {
+    tuple(sorted([(1, 0), (0, 1)])): 0,
+    tuple(sorted([(1, 0), (0, 1), (-1, 0)])): 1,
+    tuple(sorted([(-1, 0), (0, 1)])): 2,
+    tuple(sorted([(-1, 0), (0, -1), (0, 1)])): 3,
+    tuple(sorted([(-1, 0), (0, -1)])): 4,
+    tuple(sorted([(-1, 0), (0, -1), (1, 0)])): 5,
+    tuple(sorted([(1, 0), (0, -1)])): 6,
+    tuple(sorted([(1, 0), (0, -1), (0, 1)])): 7,
+    tuple(sorted([(1, 0), (-1, 0), (0, 1), (0, -1)])): 8,
+
+
+}
+
+NEIGHBOR_OFFSETS=[(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1), (0, 0), (0, 1),
+                  (1, -1), (1, 0), (1, 1)]
+
+PHYSICS_TILES = {'grass', 'stone'}
+AUTOTILE_TYPES = {'grass', 'stone'}
+import pygame
+import json
+
+class Tilemap:
+    def __init__(self, game, tile_size=16):
+        self.game = game
+        self.tile_size = tile_size
+        self.tilemap = {}
+        self.offgrid_tiles = []
+
+    def save(self, path):
+        f = open(path, 'w')
+        json.dump({'tilemap': self.tilemap, 'tile_size': self.tile_size, 'offgrid': self.offgrid_tiles}, f)
+        f.close()
+
+    def load(self, path):
+        f = open(path, 'r')
+        map_data = json.load(f)
+        f.close()
+
+        self.tilemap = map_data['tilemap']
+        self.tile_size = map_data['tile_size']
+        self.offgrid_tiles = map_data['offgrid']
+
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+        for offset in NEIGHBOR_OFFSETS:
+            check_loc = str(tile_loc[0] + offset[0]) + ";" + str(tile_loc[1] + offset[1])
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+
+        return tiles
+
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile['type'] in PHYSICS_TILES:
+                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size,
+                                         tile['pos'][1] * self.tile_size,
+                                         self.tile_size,
+                                         self.tile_size))
+        return rects
+
+
+    def autotile(self):
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            neighbors = set()
+            for shift in [(-1, 0), (0,  -1), (0, 1), (1,  0)]:
+                check_loc = f"{tile['pos'][0] + shift[0]};{tile['pos'][1] + shift[1]}"
+                if check_loc in self.tilemap:
+                    if self.tilemap[check_loc]['type'] == tile['type']:
+                        neighbors.add(shift)
+            neighbors = tuple(sorted(neighbors))
+            if (tile['type'] in AUTOTILE_TYPES) and (neighbors in AUTOTILE_MAP):
+                tile['variant'] = AUTOTILE_MAP[neighbors]
+
+    def render(self, surf, offset=(0, 0)):
+        for tile in self.offgrid_tiles:
+            surf.blit(self.game.assets[tile['type']][tile['variant']],
+                      (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1])
+                      )
+
+        for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):
+            for y in range(offset[1] // self.tile_size, (offset[1] + surf.get_height()) // self.tile_size + 1):
+                loc = f"{x};{y}"
+                if loc in self.tilemap:
+                    tile = self.tilemap[loc]
+                    surf.blit(self.game.assets[tile['type']][tile['variant']],
+                              (
+                              tile['pos'][0] * self.tile_size - offset[0],
+                              tile['pos'][1] * self.tile_size - offset[1])
+                              )
+
+
+
